feba589a46e7d0e7cfd973edcc7cd1c91bd4e2db
Afi <aconique@gmail.com>
2024-01-26 01:13:18 +0100

    speed

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
+  <SpeedInsights />
   <NuxtLoadingIndicator />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
